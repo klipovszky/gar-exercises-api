710dee4fd70bc1ccf024c7a782334656252bb7e0
Keve <k.lipovszky@gmail.com>
2025-07-12 22:06:40 +0200

    Make port dynamic for Render

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.get('/exercises', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('✅ API running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`✅ API running on port ${port}`);
 });
+
